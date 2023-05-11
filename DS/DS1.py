2d9b527057106cb8245f5ce1eec7f88e8ed2134a
Weiyan Chneg <903105509@qq.com>
2023-05-11 14:58:28 +1000

    fix readme

---
+++ DS/DS1.py
@@ -0,0 +1,24 @@
+import couchdb
+import json
+import time
+from mpi4py import MPI
+import os.path
+import math
+
+
+# use MPI to save processing time
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+begin_time = time.time()
+bytesNo = os.path.getsize("twitter-huge.json")
+# Login authentication
+admin = 'admin'
+password = 'Sjx991225'
+url = f'http://{admin}:{password}@172.26.130.209:5984/'
+
+# get couchdb instance
+couch = couchdb.Server(url)
+
+# 打开json文件导入
+with()
