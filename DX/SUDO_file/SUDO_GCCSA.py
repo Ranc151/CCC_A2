8801e5c53842fb53d91e974ec2d34969c2974275
jshi324 <jshi324@aucklanduni.ac.nz>
2023-05-12 15:39:16 +1000

    data

--- DX/SUDO_file/SUDO_GCCSA.py
+++ DX/SUDO_file/SUDO_GCCSA.py
@@ -19,7 +19,7 @@ else:
     db = couch.create(db_name)
 
 # JSON文件所在的目录路径
-json_dir = 'GCCSA_json'
+json_dir = 'new_csv/GCCSA_json'
 
 # 读取并插入每个JSON文件的数据到数据库
 for filename in os.listdir(json_dir):
@@ -29,9 +29,7 @@ for filename in os.listdir(json_dir):
 
         with open(file_path, 'r') as file:
             json_data = json.load(file)
-            result = ','.join(str(x) for x in json_data[0:3])
-            # 将JSON数据插入数据库
-            doc_id, doc_rev = db.save(result)
-            print(f'Inserted document with ID: {doc_id} and revision: {doc_rev}')
+            filename, doc_rev = db.save(json_data)
+            print(f'Inserted document with ID: {filename} and revision: {doc_rev}')
 
 print('All JSON files inserted into the database.')
