edc1d24964f90ce89cc8e6bb9f2013710a18717f
Weiyan Chneg <903105509@qq.com>
2023-05-12 18:18:48 +1000

    fix readme

--- DS/DS1.py
+++ DS/DS1.py
@@ -3,18 +3,19 @@ import json
 import time
 import os.path
 import math
-import pandas
+import pandas as pd
 
 # Login authentication
 admin = 'admin'
 password = 'Sjx991225'
 url = f'http://{admin}:{password}@172.26.130.209:5984/'
-
+server=couchdb.Server(url)
 # get couchdb instance
-couch = couchdb.Server(url)
+db = server['test']
 
-# 打开json文件导入
-db = couch['twitter']
-mydocument = db.get('twitter')
-mydata = json.loads(mydocument)
-print(mydata)
+data = {'time': []}
+df = pd.DataFrame(data)
+for doc in db.view('_all_docs',include_docs=True):
+    time = doc.doc['key'][:3]
+    df = df.append({'time': time}, ignore_index=True)
+print(df)
