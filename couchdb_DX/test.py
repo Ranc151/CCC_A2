b0ef92c7a9045759bdb03226834a97da397052ba
XUHENGL <1064921660@qq.com>
2023-05-16 19:50:23 +1000

    by searching city names to create a database

--- couchdb_DX/test.py
+++ couchdb_DX/test.py
@@ -2,8 +2,15 @@ import couchdb
 import json
 import time
 from mpi4py import MPI
+import os.path
+import math
 
-
+# use MPI to save processing time
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+begin_time = time.time()
+bytesNo = os.path.getsize("G:\\py\\twitter-huge.json")
 # Login authentication
 admin = 'admin'
 password = 'Sjx991225'
@@ -13,64 +20,60 @@ url = f'http://{admin}:{password}@172.26.130.209:5984/'
 couch = couchdb.Server(url)
 
 # set the db name
-db_name = 'twitter'
+db_name = 't'
+db = couch[db_name]
 
-# Whether the database exists
-if db_name in couch:
-    db = couch[db_name]
-else:
-    db = couch.create(db_name)
+# divide the file into parts and then read the file in parallel using MPI
+startIndex = math.floor(bytesNo / size) * rank
+endIndex = startIndex + math.floor(bytesNo / size)
 
 
 # start to store the data
-with open('twitter-huge.json', 'r', encoding='utf-8') as file:
+with open("G:\\py\\twitter-huge.json", 'r', encoding='utf-8') as file:
+    if rank == 0:  # rank 0 is the first part of this file so need ignore the '[' at the beginning of
+        new_line = file.readline()  # ignore the '[' at the beginning of twitter-data-small.json
+    file.seek(startIndex)  # Read in from the assigned part
 
-    for i in range(5):
+    while True:
         new_line = file.readline()
         if new_line != "]}":
-            keyword_area = ['sydney', 'melbourne', 'brisbane', 'perth', 'adelaide', 'hobart', 'darwin', 'canberra', 'australia', "ta"]
-            keyword_epidemic = ['epidemic', 'virus', 'coronavirus', 'COVID-19', 'vaccine',
-                                'preventative measures', 'mask', 'social distancing', 'testing',
-                                'quarantine', 'lockdown', 'outbreak', 'cases', 'death toll', 'recovery',
-                                'state of emergency', "ta"]
-            keyword_time = ["2020", "2021", "2022", "ta"]
-            keyword = ['unemployment rate', 'employment rate', 'job seekers', 'job vacancies', 'career transition',
-                       'labor market', 'salary',
-                       'full-time', 'part-time', 'self-employment', 'vocational training',
-                       'employment opportunities', 'labor force participation rate', 'rent', 'apartment', 'house',
-                       'lease',
-                       'landlord', 'tenant',
-                       'real estate', 'property management', 'security deposit', 'utilities',
-                       'rental agreement', 'eviction', 'rental market', 'rental income', 'cost of living', 'inflation',
-                       'consumer price index',
-                       'price level', 'wage growth', 'price stability', 'deflation',
-                       'living expenses', 'food prices', 'energy prices', 'housing costs',
-                       'transportation costs', 'healthcare costs', 'education costs',
-                       'childcare costs', 'retirement savings', 'fever', 'cough', 'sore throat', 'nasal congestion',
-                       'runny nose', 'sneezing', 'body aches', 'fatigue', 'loss of appetite', 'headache', 'chills',
-                       'employed full-time', 'employed part-time', 'not in labour force', 'unemployed total',
-                       '15 to 24', '25 to 34', '35 to44',
-                       '45 to 54', '55 to 64', '65 and over']
-            stop_all_loops = False
-            for i in keyword_area:
-                if i in new_line.lower():
-                    for j in keyword_epidemic:
-                        if j in new_line.lower():
-                            for p in keyword_time:
-                                if p in new_line.lower():
-                                    for t in keyword:
-                                        if t in new_line.lower():
-                                            stop_all_loops = True
-                                            print(new_line)
-                                             # load a json string to dict
+            cities =['Central Coast', 'Sydney - Baulkham Hills and Hawkesbury', 'Sydney - Blacktown',
+                     'Sydney - City and Inner South', 'Sydney - Eastern Suburbs', 'Sydney - Inner South West',
+                     'Sydney - Inner West', 'Sydney - North Sydney and Hornsby', 'Sydney - Northern Beaches',
+                     'Sydney - Outer South West', 'Sydney - Outer West and Blue Mountains', 'Sydney - Parramatta',
+                     'Sydney - Ryde', 'Sydney - South West', 'Sydney - Sutherland', 'Capital Region', 'Central West',
+                     'Coffs Harbour - Grafton', 'Far West and Orana', 'Hunter Valley exc Newcastle', 'Illawarra',
+                     'Mid North Coast', 'Murray', 'New England and North West', 'Newcastle and Lake Macquarie',
+                     'Richmond - Tweed', 'Riverina', 'Southern Highlands and Shoalhaven', 'Melbourne - Inner',
+                     'Melbourne - Inner East', 'Melbourne - Inner South', 'Melbourne - North East',
+                     'Melbourne - North West', 'Melbourne - Outer East', 'Melbourne - South East',
+                     'Melbourne - West', 'Mornington Peninsula', 'Ballarat', 'Bendigo', 'Geelong',
+                     'Hume', 'Latrobe - Gippsland', 'North West', 'Shepparton', 'Warrnambool and South West',
+                     'Brisbane - East', 'Brisbane - North', 'Brisbane - South', 'Brisbane - West',
+                     'Brisbane Inner City', 'Ipswich', 'Logan - Beaudesert', 'Moreton Bay - North',
+                     'Moreton Bay - South', 'Cairns', 'Darling Downs - Maranoa', 'Central Queensland',
+                     'Gold Coast', 'Mackay - Isaac - Whitsunday', 'Queensland - Outback', 'Sunshine Coast',
+                     'Toowoomba', 'Townsville', 'Wide Bay', 'Adelaide - Central and Hills', 'Adelaide - North',
+                     'Adelaide - South', 'Adelaide - West', 'Barossa - Yorke - Mid North', 'South Australia - Outback',
+                     'South Australia - South East', 'Mandurah', 'Perth - Inner', 'Perth - North East',
+                     'Perth - North West', 'Perth - South East', 'Perth - South West', 'Bunbury',
+                     'Western Australia - Wheat Belt', 'Hobart', 'Launceston and North East', 'South East',
+                     'West and North West', 'Darwin', 'Northern Territory - Outback','NSW', 'VIC', 'QLD', 'SA', 'WA', 'TAS', 'NT']
 
-                                            # analyse the twitter
-                                            break
-                                    if stop_all_loops:
-                                        break
-                            if stop_all_loops:
-                                break
-                    if stop_all_loops:
-                        break
+            for city in cities:
+                if city in new_line:
+                    twitter = json.loads(new_line[:-2])
+                    t_id = twitter.get("id")
+                    doc = db.get("_id")
+                    if db.get(t_id):
+                        rev = db.get(t_id).rev
+                        twitter["_id"] = t_id
+                        twitter["_rev"] = rev
+                    else:
+                        twitter["_id"] = t_id
+                    doc_id, doc_rev = db.save(twitter)  # analyse the twitter
+                    break
         else:
             break
+if rank == 0:
+    print(time.time() - begin_time)
