600af2a716173ff6c87e2f49e239ec6a704f99ad
jshi324 <jshi324@aucklanduni.ac.nz>
2023-05-21 17:05:10 +1000

    data

--- couchdb_DX/SUDO_file/new_csv/GCCSA_json/geo_json.py
+++ couchdb_DX/SUDO_file/new_csv/GCCSA_json/geo_json.py
@@ -1,16 +1,13 @@
 import json
 
-json_file = 'Labour_2021.json'  # 输入的CSV文件路径
-
 # 读取 JSON 文件
-with open('Labour_2021.json', "r") as f:
+with open('Labour_2020.json', "r") as f:
     json_data = json.load(f)
-
 
 # Create the GeoJSON data structure
 geojson_data = {
-  "type": "FeatureCollection",
-  "features": [json_data]
+    "type": "FeatureCollection",
+    "features": [json_data]
 }
 
 # Save the GeoJSON data to a file
